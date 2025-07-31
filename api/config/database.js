40fdd09b638485030ebd94345a67ecab4b1ad7c3
Khevynn Mychael Cardoso de Sá <115984821+Khevynn@users.noreply.github.com>
2025-07-31 17:32:12 +0100

    Update database.js

--- api/config/database.js
+++ api/config/database.js
@@ -3,18 +3,18 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(
   process.env.DB_NAME, // database name
   process.env.DB_USER, // username
-   // password
-    {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT, // specify port
-      dialect: "mysql",
-      pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
-      },
-      logging: false, // or true for SQL debug output
+  process.env.DB_PASSWORD, // password
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT, // specify port
+    dialect: "mysql", // explicitly specify the dialect
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+    logging: false, // or true for SQL debug output
   }
 );
 
